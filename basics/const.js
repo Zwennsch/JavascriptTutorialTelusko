8af31b5a40e65708579183b86144441c619d206d
Sven Schroeder <schuen@gmx>
2021-05-10 10:45:44 +0200

    basics of constants and variables

---
+++ basics/const.js
@@ -0,0 +1,12 @@
+const pi = 3.14
+let radius = 5
+let area
+
+area = pi * radius * radius
+
+console.log(area)
+
+radius = 3.5
+area = pi * radius * radius
+
+console.log(area)
---
+++ basics/variables.js
@@ -0,0 +1,7 @@
+num = 2+4
+console.log(num)
+console.log(num +6)
+// better:
+let num2 = 3+6
+let username = "Sven"
+console.log(username)
