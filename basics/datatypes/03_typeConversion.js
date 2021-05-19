9ef174724bc30815e6cdcf5dff09e1eef5befa38
Sven Schroeder <schuen@gmx.de>
2021-05-19 15:32:10 +0200

    type conversion

---
+++ basics/datatypes/03_typeConversion.js
@@ -0,0 +1,28 @@
+// let num = 6
+// console.log(typeof(num), num) // Number 6
+
+// let word = String(6)
+// console.log(typeof(word))   // String
+// let w2 = "123" 
+// console.log(typeof(w2))     //String
+// let num2 = Number(w2);
+// console.log(typeof(num2))   //Number
+
+
+// let x
+// console.log(x, typeof(x))   //undefined, undefined
+// x = 8
+// console.log(x, typeof(x))   //8, Number
+// x = x + ""
+// console.log(x, typeof(x))   //8, String
+
+let y = "123 Sven"
+console.log(y)
+y = Number(y)
+console.log(y)
+console.log(typeof(y))
+let s = "123 Sven"
+console.log(s)
+console.log(typeof(s))
+let t = parseInt(s)
+console.log(t)
