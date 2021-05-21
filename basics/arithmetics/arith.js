cee26e122969c083c8ae2556240c34bb2009b0b7
Sven Schroeder <schuen@gmx.de>
2021-05-21 09:26:03 +0200

    simple math operations

---
+++ basics/arithmetics/arith.js
@@ -0,0 +1,20 @@
+let num1 = 4
+let num2 = 7
+let b = true
+let a = true
+let word = "hallo"
+
+// console.log(typeof(num1))
+// console.log(typeof(word))
+// console.log(typeof(b))
+
+let result = a + b
+let x = num2++
+let y = Math.pow(num1, 3)
+let y2 = num1 ** 3 // shorthand for power of...
+
+console.log(result) // =>2  ;because of type conversion it gets convert into 1 + 1;
+console.log(x, num2)    // 7, 8
+console.log(y)
+console.log(y2)
+
