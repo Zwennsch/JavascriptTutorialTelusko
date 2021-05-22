a5cdbe87255ba8296b7bb1b7a02059ef0a6609d1
Sven Schroeder <schuen@gmx.de>
2021-05-22 09:02:35 +0200

    relational stuff

---
+++ basics/arithmetics/relational.js
@@ -0,0 +1,17 @@
+let x = "Sven"
+let y = "Anke"
+
+console.log(x>y)
+
+// it compares each character one by one, because each character has an ASCII value
+let a = "2"
+let b = 1
+
+console.log(a >b)
+//it works because a gets converted automatically into a number
+
+let one = "1"
+let two = 1
+
+console.log(one == two)
+console.log(one === two) //this compares the type equality
