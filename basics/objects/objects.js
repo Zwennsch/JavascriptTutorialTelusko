75407824bc86fd7d1838762f38ad796d5f60266f
Sven Schroeder <schuen@gmx.de>
2021-06-05 10:48:41 +0200

    objects first

---
+++ basics/objects/objects.js
@@ -0,0 +1,15 @@
+let input = "name"
+
+let alien = {
+    name : "Hans",
+    technology : "JS",
+
+}
+
+console.log(typeof(alien));
+console.log(alien);
+console.log(alien.name);
+// the same with []
+console.log(alien["name"]);
+// normally you would use the . operator
+console.log(alien[input]);
