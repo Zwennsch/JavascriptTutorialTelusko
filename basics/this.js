eb4d5d24a3452b338ba6eedaac4696c1195460c2
Sven Schroeder <schuen@gmx.de>
2021-06-24 14:18:41 +0200

    this in functions

--- basics/this.js
+++ basics/this.js
@@ -11,3 +11,23 @@ let myObject = {
 }
 myObject.greet();
 
+let laptop1 = {
+    cpu: 'i7',
+    brand: 'HP',
+    ram: 16,
+
+    compare : function(otherLaptop){
+        if(this.cpu > otherLaptop.cpu){
+            console.log(this);
+        }else{console.log(otherLaptop);}
+    }
+}
+
+let laptop2 = {
+    cpu: 'i5',
+    brand: 'Dell',
+    ram: 8
+}
+
+laptop1.compare(laptop2);
+
